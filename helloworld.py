41890fb55f2542f3d7895f7c1f34bbcee6fccce8
YarvalCoder <60534325+YarvalCoder@users.noreply.github.com>
2020-02-08 11:22:47 +0530

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,13 @@
+print('Hello World')
+
+
+# ______Data Types in Python_____
+# 'Hello' --> string str
+# 45 --> integer int
+# '45' --> string str
+# 34.56 --> float
+# True or False --> Boolean bool
+# 'True' or 'False' --> string str
+
+# Commenting of lines
+# print ('Hey')
